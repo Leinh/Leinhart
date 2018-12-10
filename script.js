512e757f28be65f0aba68abb3b14e83bd00547b9
Leinh <leinhou@gmail.com>
2018-12-10 09:53:28 +0100

    Derniers ajouts sur le jeu et corrections de bugs

--- script.js
+++ script.js
@@ -28,49 +28,50 @@ function change() {
 
 function game(choixjoueur) {
     var choix_ia = Math.floor(Math.random() * 3);
+    document.getElementById("resultat").style.display = "block";
 
     if (choixjoueur == choix_ia) {
-       
-       
+
+
         if (a == 1) {
             if (choix_ia == 0) {
                 document.getElementById("choixia").innerHTML = '<img src="images/sith_warrior.png" height="300px" width="300px">';
-                document.getElementById("choixia").style = " border:2px solid gold; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                 document.getElementById("choixia").display = "block";
-                document.getElementById("resultat").display = "block";
+               
             }
             else if (choix_ia == 1) {
                 document.getElementById("choixia").innerHTML = '<img src="images/imperial_agent.png" height="300px" width="300px">';
-                document.getElementById("choixia").style = " border:2px solid gold; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                 document.getElementById("choixia").display = "block";
-                document.getElementById("resultat").display = "block";
+                
             }
             else {
                 document.getElementById("choixia").innerHTML = '<img src="images/sith_sorcerer.png" height="300px" width="150px">';
-                document.getElementById("choixia").style = " border:2px solid gold; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                 document.getElementById("choixia").display = "block";
-                document.getElementById("resultat").display = "block";
+               
             }
         }
 
         else {
             if (choix_ia == 0) {
                 document.getElementById("choixia").innerHTML = '<img src="images/jedi_warrior.png" height="300px" width="180px">';
-                document.getElementById("choixia").style = " border:2px solid gold; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                 document.getElementById("choixia").display = "block";
-                document.getElementById("resultat").display = "block";
+               
             }
             else if (choix_ia == 1) {
                 document.getElementById("choixia").innerHTML = '<img src="images/republic_trooper.png" height="300px" width="220px">';
-                document.getElementById("choixia").style = " border:2px solid gold; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                 document.getElementById("choixia").display = "block";
-                document.getElementById("resultat").display = "block";
+                
             }
             else {
-                document.getElementById("choixia").innerHTML = '<img src="images/jedi_sage.png" height="300px" width="230px">';
-                document.getElementById("choixia").style = " border:2px solid gold; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                document.getElementById("choixia").innerHTML = '<img src="images/jedi_sage.png" height="300px" width="200px">';
+                document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                 document.getElementById("choixia").display = "block";
-                document.getElementById("resultat").display = "block";
+               
             }
         }
 
@@ -87,30 +88,30 @@ function game(choixjoueur) {
                     document.getElementById("choixia").innerHTML = '<img src="images/imperial_agent.png" height="300px" width="300px">';
                     document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                    
                 }
 
                 else {
                     document.getElementById("choixia").innerHTML = '<img src="images/sith_sorcerer.png" height="300px" width="150px">';
-                    document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                    document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                    
                 }
             }
 
             else {
                 if (choix_ia == 1) {
                     document.getElementById("choixia").innerHTML = '<img src="images/republic_trooper.png" height="300px" width="220px">';
-                    document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                    document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                    
                 }
 
                 else {
-                    document.getElementById("choixia").innerHTML = '<img src="images/jedi_sage.png" height="300px" width="230px">';
+                    document.getElementById("choixia").innerHTML = '<img src="images/jedi_sage.png" height="300px" width="200px">';
                     document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                    
                 }
 
             }
@@ -125,30 +126,30 @@ function game(choixjoueur) {
                     document.getElementById("choixia").innerHTML = '<img src="images/sith_sorcerer.png" height="300px" width="150px">';
                     document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                    
                 }
 
                 else {
                     document.getElementById("choixia").innerHTML = '<img src="images/sith_warrior.png" height="300px" width="300px">';
-                    document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                    document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                    
                 }
             }
 
             else {
                 if (choix_ia == 2) {
-                    document.getElementById("choixia").innerHTML = '<img src="images/jedi_sage.png" height="300px" width="230px">';
-                    document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                    document.getElementById("choixia").innerHTML = '<img src="images/jedi_sage.png" height="300px" width="200px">';
+                    document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                    
                 }
 
                 else {
                     document.getElementById("choixia").innerHTML = '<img src="images/jedi_warrior.png" height="300px" width="180px">';
                     document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                   
                 }
 
             }
@@ -167,30 +168,30 @@ function game(choixjoueur) {
                     document.getElementById("choixia").innerHTML = '<img src="images/sith_warrior.png" height="300px" width="300px">';
                     document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                    
                 }
 
                 else {
                     document.getElementById("choixia").innerHTML = '<img src="images/imperial_agent.png" height="300px" width="300px">';
-                    document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                    document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                   
                 }
             }
 
             else {
                 if (choix_ia == 0) {
                     document.getElementById("choixia").innerHTML = '<img src="images/jedi_warrior.png" height="300px" width="180px">';
-                    document.getElementById("choixia").style = " border:2px solid red; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
+                    document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                    
                 }
 
                 else {
                     document.getElementById("choixia").innerHTML = '<img src="images/republic_trooper.png" height="300px" width="220px">';
                     document.getElementById("choixia").style = " border:2px solid lightblue; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; ";
                     document.getElementById("choixia").display = "block";
-                    document.getElementById("resultat").display = "block";
+                    
                 }
 
 
@@ -204,14 +205,14 @@ function game(choixjoueur) {
 
         }
     }
-    
+
 
     setTimeout(
         function () {
             document.getElementById("choixia").style.display = "none"
         }, 2000);
 
-    
+
 }
 
 var vie = 5;
@@ -226,12 +227,13 @@ function resultat(t) {
         
 
 
+
     }
 
     else if (t == "victoire") {
 
         document.getElementById("resultat").innerHTML = "You won this battle.";
-        document.getElementById("resultat").display="block";
+        
 
         vieia--;
     }
@@ -239,22 +241,25 @@ function resultat(t) {
     else {
 
         document.getElementById("resultat").innerHTML = "You lost this battle.";
-        document.getElementById("resultat").display="block";
+       
 
         vie--;
 
     }
 
     if (vieia == 0 || vie == 0) {
+        document.getElementById("resultat").display="block";
+        document.getElementById('choixia').display = "none";
 
         if (vieia == 0) {
-            document.getElementById("resultat").innerHTML = "You have won the war.";
-            document.getElementById("resultat").display="block";
+            document.getElementById("finalresult2").innerHTML = "You have won the war.";
+            
+            
         }
 
         else {
-            document.getElementById("resultat").innerHTML = "You have lost the war.";
-            document.getElementById("resultat").display="block";
+            document.getElementById("finalresult2").innerHTML = "You have lost the war.";
+           
         }
 
         document.getElementById("finalresult").innerHTML = ' Come back and fight!';
@@ -262,28 +267,22 @@ function resultat(t) {
         document.getElementById("finalresult").display = "block";
 
 
-        document.getElementById('choixia').display = "none";
+        
     }
 
     setTimeout(
         function () {
             document.getElementById("resultat").style.display = "none"
         }, 2000);
+        
 
 
     document.getElementById("scoreia").innerHTML = vieia;
     document.getElementById("scorejoueur").innerHTML = vie;
 
-    
+
 
 }
-
-
-
-
-
-
-
 
 
 function restart() {
